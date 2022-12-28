b5b9b3e20fa1be3100d73ca157bea85bf5ba7537
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2022-12-28 11:05:52 +0900

    複数テストファイルをいじる（この後マージイベント発火させる）

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -3,7 +3,22 @@
 require "rails_helper"
 
 RSpec.describe "hoge" do
-  context "when calculated" do
+  context "when calculated 1" do
+    subject { 1 + 2 }
+    it { is_expected.to eq(3) }
+  end
+
+  context "when calculated 2" do
+    subject { 1 + 2 }
+    it { is_expected.to eq(3) }
+  end
+
+  context "when calculated 3" do
+    subject { 1 + 2 }
+    it { is_expected.to eq(3) }
+  end
+
+  context "when calculated 4" do
     subject { 1 + 2 }
     it { is_expected.to eq(3) }
   end
