9478068c64ccffeb98c20ba5f86188fe4c73c923
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2022-12-28 12:31:02 +0900

    マージ時にマージ者の名前を取得する方法がわかったので更新2

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe "hoge" do
   context "when calculated" do
-    subject { 1 + 2 }
-    it { is_expected.to eq(3) }
+    subject { 1 + 3 }
+    it { is_expected.to eq(4) }
   end
 end
