24f4537926772ed20a637507ae7e520c7f6e5490
takahiro_umeda <takahiro.umeda@pixta.co.jp>
2023-02-14 11:31:51 +0900

    some-branch-202302141130

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -6,5 +6,19 @@ RSpec.describe "hoge" do
   context "when calculated" do
     subject { 1 + 3 }
     it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
+    it { is_expected.to eq(4) }
   end
 end
